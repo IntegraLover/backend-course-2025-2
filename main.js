9204fceb3b90b49db958bf52fb3286a0d3c646bb
Volodymyr Mendelykh <volodymyr.mendelykh@lnu.edu.ua>
2025-09-26 16:01:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-﻿// main.js for part 3
-
 
 // Варіант 2 — доходи та витрати банків
-const date = "202408";        // дата
-const time_period = "m";      // період
+const date = "202408";        
+const time_period = "m";     
 
-// Базовий URL НБУ
+
 const baseUrl = "https://bank.gov.ua/NBUStatService/v1/statdirectory/banks_income_expenses";
 
 // Створюємо обʼєкт URL
 const url = new URL(baseUrl);
 
-// Додаємо параметри через searchParams
+
 url.searchParams.append("date", date);
 url.searchParams.append("period", time_period);
 url.searchParams.append("json", "");
 
-// Виводимо URL у консоль
+
 console.log("Constructed URL:", url.toString());
+
